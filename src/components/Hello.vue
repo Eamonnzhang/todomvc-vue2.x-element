31b615b0e03629555aa5c4556f904f918919cfcf
EamonnZhang <5eamonn99@gmail.com>
2016-12-06 13:48:42 +0800

    初步搭建webpack2.x+vue2.x环境

---
+++ src/components/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="hello">
+        <h2>Hello.vue</h2>
+        <p>{{message}}
+            <span>yellow</span>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'hello',
+        data(){
+            return {
+                message : 'Hello.vue message'
+            }
+        }
+    }
+</script>
+
+<style lang="sass">
+    h2{
+        font-size: 16px;
+        color: red;
+        p {
+            color: blue;
+        }
+    }
+    p{
+        text-align: center;
+        span {
+            color : yellow;
+        }
+    }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry : './src/main.js',
+    output : {  
+        path : path.resolve(__dirname,'./dist'),
+        publicPath : '/dist/',
+        filename : 'bundle.js'
+    },
+    module : {
+        rules : [
+            {
+                test : /\.vue$/,
+                loader : 'vue-loader',
+                options : {
+                     scss: 'style!css!sass'
+                }
+            },
+            {
+                test : /\.js$/,
+                loader : 'babel-loader',
+                exclude : /node_modules/
+            },
+            {
+                test : /\.(png|jpg|gif|svg)/,
+                loader : 'file-loader',
+                options : {
+                    name : '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve : {
+        alias : {
+
+        }
+    },
+    devServer : {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    devtool : '#eval-source-map'
+}
+
+if(process.env.NODE_ENV === 'production'){
+    module.exports.devtool = '#source-map'
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env' : {
+                NODE_ENV : '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap :true,
+            compress : {
+                warnings : false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize : true
+        })
+    ])
+}
