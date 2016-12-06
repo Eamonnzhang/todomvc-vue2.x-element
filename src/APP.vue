a9fc3f766d994e0f665f0a516d4ac8469d6f8de4
Eamonnzhang <394997632@qq.com>
2016-12-06 17:21:58 +0800

    加入过滤功能

--- src/APP.vue
+++ src/APP.vue
@@ -2,33 +2,41 @@
      <div id="app">
          <h2>{{msg}}</h2>
         <my-input></my-input>
-        <todo-list :todos=todos></todo-list>
+        <todo-list :todos="todos" :filter="filter"></todo-list>
+        <todo-footer></todo-footer>
      </div>
 </template>
 
 <script>    
 import MyInput from './components/Input.vue';
 import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
 import {eventHub} from './components/EventHub.js';
 export default {
     name : 'app',
     data () {
         return {
-            todos : ['abc'],
+            todos : [],
+            filter : 'all',
             msg:'Vue2.x TodoMVC'
         }
     },
     created(){
         eventHub.$on('add-todo',this.addTodo)
+        eventHub.$on('set-filter',this.setFilter)
     },
     methods : {
         addTodo(newTodo){
-            this.todos.push(newTodo)
+            this.todos.push({value:newTodo,completed:false})
+        },
+        setFilter(newfilter){
+            this.filter = newfilter
         }
     },
     components :{
         MyInput,
-        TodoList
+        TodoList,
+        TodoFooter
     }
 }
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,35 +1,57 @@
 <template>
     <div class="km-todolist">
         <ol>
-            <li v-for="(item,index) in todos">
-                {{item}}
-                <button @click="removeTodo(index)">X</button>
+            <li v-for="(item,index) in filterTodos" :class="{complete:item.completed}">
+                <span>{{item.value}}</span>
+                <button @click="removeTodo(index)">删除</button>
+                <button @click="toggleTodo(item)" :class="{complete:item.completed}">完成</button>
             </li>        
         </ol>
     </div>
 </template>
 
 <script>
+    let filter = {
+        all:function(todos){
+            return todos;
+        },
+        active:function(todos){
+            return todos.filter(function(todo){
+                return !todo.completed
+            })
+        },
+        completed:function(todos){
+             return todos.filter(function(todo){
+                return todo.completed
+            })
+        },
+    }
     export default {
         name : 'todo-list',
-        props : ['todos'],
-        data(){
-            return {
-                list : [
-                    'item1',
-                    'item2',
-                    'item3'
-                ]
-            }
-        },
+        props : ['todos','filter'],
         methods : {
             removeTodo(index){
                 this.todos.splice(index,1)
             },
+            toggleTodo(item){
+                item.completed = !item.completed
+            }
+        },
+        computed : {
+            filterTodos(){
+                return filter[this.filter](this.todos)
+            }
         }
     }
 </script>
 
 <style lang="sass">
-
+    li.complete{
+        span{
+            text-decoration: line-through;
+        }
+    }
+    button.complete{
+        background-color: #fff;
+    }
 </style>
