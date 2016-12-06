e42518d9be3b263ce34660bcfc70bb6a9c7477d5
Eamonnzhang <394997632@qq.com>
2016-12-06 16:13:03 +0800

    完成添加和删除Todo

---
+++ src/components/Input.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="km-input-wrap">
+        <input @keyup.enter="addNewTodo" placeholder="add new todo" v-model='todoItem'>
+    </div>
+</template>
+
+<script>
+import TodoList from './TodoList.vue';
+import Vue from 'vue';
+import {eventHub} from './EventHub.js';
+export default {
+    name : 'my-input',
+    data(){
+        return {
+            todoItem:''
+        }
+    },
+    methods : {
+        addNewTodo(){
+            eventHub.$emit('add-todo',this.todoItem);
+            this.todoItem = ''
+        }
+    },
+}
+</script>
+
+<style lang="sass">
+
+</style>
