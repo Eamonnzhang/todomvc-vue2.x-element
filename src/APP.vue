774875f6f8b04ffefde060054330303bfb263104
Eamonnzhang <394997632@qq.com>
2016-12-06 13:48:42 +0800

    初步搭建webpack2.x+vue2.x环境

---
+++ src/APP.vue
@@ -0,0 +1,25 @@
+<template>
+     <div id="app">
+        <h1>{{msg}}</h1>
+        <hello></hello>
+     </div>
+</template>
+
+<script>    
+import Hello from './components/Hello.vue';
+export default {
+    name : 'app',
+    data () {
+        return {
+            msg: 'TodoMVC-Vue2.x'
+        }
+    },
+    components :{
+        Hello
+    }
+}
+</script>
+
+<style lang="sass"> 
+
+</style>
